a06272784d020f9a23783bcf95d999ecebeaa1be
Gustavo Firmino <gustavo.barbosa@dmk3.com.br>
2026-05-27 15:10:12 -0300

    feat: Implementação da validação de email

--- back/backend/src/app.module.ts
+++ back/backend/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { UsuarioController } from './usuario/usuario.controller';
 import { UsuarioService } from './usuario/usuario.service';
 import { UsuarioModule } from './usuario/usuario.module';
+import { VerificarEmail } from './usuario/dto/validacao/verificar-email';
 
 @Module({
   imports: [UsuarioModule],
   controllers: [UsuarioController],
-  providers: [UsuarioService],
+  providers: [UsuarioService, VerificarEmail],
 })
 export class AppModule {}
